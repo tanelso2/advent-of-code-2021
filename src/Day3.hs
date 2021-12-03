9894f9acadfe0c95fd571f6b37c88b701d935eb0
Thomas Nelson <tanelso2@gmail.com>
2021-12-03 10:47:17 -0600

    Done with day3

--- src/Day3.hs
+++ src/Day3.hs
@@ -5,16 +5,99 @@ module Day3
 
 import Lib
 
-type Day3Input = ()
+import Data.List
+
+type Day3Input = String
 
 parseInput :: String -> Either () Day3Input
-parseInput = \x -> Right ()
+parseInput = Right
 
 doDay3 :: IO ()
-doDay3 = doDay 3 parseInput part1 part2
+doDay3 = doDayWTest 3 parseInput part1 part2 Nothing
+
+columns' :: [[a]] -> [[a]]
+columns' xs = foldr1 f $ map (map (:[])) xs
+  where
+    f line acc = zipWith (++) line acc
+
+columns :: String -> [String]
+columns s = foldr1 f $ map (map (:[])) $ lines s
+  where
+    f line acc = zipWith (++) line acc
+
+bitFlip :: [Char] -> [Char]
+bitFlip [] = []
+bitFlip ('0':xs) = '1':(bitFlip xs)
+bitFlip ('1':xs) = '0':(bitFlip xs)
+bitFlip _ = error "Bitflip failed"
+
+readBinary :: String -> Int
+readBinary s = snd $ foldr f (1,0) s
+  where
+    f '0' (place,acc) = (place*2,acc)
+    f '1' (place,acc) = (place*2,acc+place)
+    f _ _ = error "readBinary was sent something that wasn't 1 or 0"
+
+mostCommon :: [Char] -> Char
+mostCommon xs = if o >= z then '1' else '0'
+  where
+    o = length $ filter (=='1') xs
+    z = length $ filter (=='0') xs
+
+mostCommonBin :: [String] -> String
+mostCommonBin = (map mostCommon) . columns'
+
+leastCommonBin = bitFlip . mostCommonBin
 
 part1 :: Day3Input -> Result Int
-part1 xs = NotImpl
+part1 s = Res $ epsilon * gamma
+  where
+    gamma = readBinary binGamma
+    epsilon = readBinary binEpsilon
+    binEpsilon = bitFlip binGamma
+    binGamma = map mostCommon $ columns s
+
+getCandidate :: String -> [String] -> String
+getCandidate x xs = c
+  where
+    c = f $ sort $ x:xs
+    f (b:c:xs) = if b == x then c else f (c:xs)
 
 part2 :: Day3Input -> Result Int
-part2 xs = NotImpl
+part2 s = Res $ rO2 * rCO2
+  where
+    bins = lines s
+    binO2 = filterDownBy mostCommonBin bins 0
+    binCO2 = filterDownBy leastCommonBin bins 0
+    rO2 = readBinary binO2
+    rCO2 = readBinary binCO2
+
+filterDownBy :: ([String] -> String) -> [String] -> Int -> String
+filterDownBy _ [a] _ = a
+filterDownBy f xs n =
+  filterDownBy f results (n+1)
+  where
+    x = f xs
+    results = filter (\z -> (z !! n) == (x !! n)) xs
+
+exampleInput = [
+  "00100"
+  , "11110"
+  , "10110"
+  , "10111"
+  , "10101"
+  , "01111"
+  , "00111"
+  , "11100"
+  , "10000"
+  , "11001"
+  , "00010"
+  , "01010"
+  ]
+
+day3testing :: Day3Input -> IO ()
+day3testing s = do
+  bins <- return $ lines s
+  binO2 <- return $ filterDownBy mostCommonBin bins 0
+  putStrLn $ "binO2: " ++ show binO2
+  putStrLn $ "Example input: " ++ show (filterDownBy mostCommonBin exampleInput 0)
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,6 +2,7 @@ module Lib
     (
     Result(..)
     , doDay
+    , doDayWTest
     ) where
 
 import Control.Exception
@@ -19,8 +20,10 @@ printPart dayNum partNum res = do
 
 filename n = "inputs/" ++ "day" ++ show n ++ ".input"
 
-doDay :: (Show a, Show b, Show e) => Int -> (String -> Either e i) -> (i -> Result a) -> (i -> Result b) -> IO ()
-doDay n parseInput part1 part2 = do
+doDay n parseInput p1 p2 = doDayWTest n parseInput p1 p2 Nothing
+
+doDayWTest :: (Show a, Show b, Show e) => Int -> (String -> Either e i) -> (i -> Result a) -> (i -> Result b) -> Maybe (i -> IO ()) -> IO ()
+doDayWTest n parseInput part1 part2 maybeTest = do
   rawOrExc <- try $ readFile $ filename n :: IO (Either IOException String)
   case rawOrExc of
     Left ex -> return () -- putStrLn $ "  Exception: " ++ show ex
@@ -29,6 +32,11 @@ doDay n parseInput part1 part2 = do
       case parseInput raw of
         Left e -> putStrLn $ "ParseError: " ++ show e
         Right input -> do
+          case maybeTest of
+            Nothing -> return ()
+            Just f -> do
+              putStrLn " - TESTING - "
+              f input
           maybeDo (part1 input) $ printPart n 1
           maybeDo (part2 input) $ printPart n 2
       putStrLn ""
