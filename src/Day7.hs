55c7aa2e0a3d546460fc420033a8666b2b0ceb96
Thomas Nelson <tanelso2@gmail.com>
2021-12-07 10:05:00 -0600

    Day 7 completed

--- src/Day7.hs
+++ src/Day7.hs
@@ -4,17 +4,41 @@ module Day7
   ) where
 
 import Lib
+import ParseUtils
 
-type Day7Input = ()
+import Text.ParserCombinators.Parsec
 
-parseInput :: String -> Either () Day7Input
-parseInput = \x -> Right ()
+parseDay7 :: Parser Day7Input
+parseDay7 = sepBy1 int $ char ','
+
+type Day7Input = [Int]
+
+parseInput :: String -> Either ParseError Day7Input
+parseInput = parse parseDay7 ""
 
 doDay7 :: IO ()
 doDay7 = doDay 7 parseInput part1 part2
 
+fuelCost :: [Int] -> Int -> Int
+fuelCost crabs dest = sum $ map (distance dest) crabs
+
 part1 :: Day7Input -> Result Int
-part1 xs = NotImpl
+part1 xs = Verified 359648 $ foldr1 min $ map (fuelCost xs) $ [lb..ub]
+  where
+    ub = foldr1 max xs
+    lb = foldr1 min xs
+
+costForDistance :: Int -> Int
+costForDistance n = (n * (n+1)) `div` 2
+
+distance :: Int -> Int -> Int
+distance a b = abs $ a - b
+
+fuelCost' :: [Int] -> Int -> Int
+fuelCost' crabs dest = sum $ map (costForDistance . (distance dest)) crabs
 
 part2 :: Day7Input -> Result Int
-part2 xs = NotImpl
+part2 xs = Verified 100727924 $ foldr1 min $ map (fuelCost' xs) $ [lb..ub]
+  where
+    ub = foldr1 max xs
+    lb = foldr1 min xs
