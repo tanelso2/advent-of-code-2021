0ab82e8dff80bce02b7aeb88c65e1e8fdcd80283
Thomas Nelson <tanelso2@gmail.com>
2021-12-09 19:28:47 -0600

    Done with day9 part1

--- src/Day9.hs
+++ src/Day9.hs
@@ -5,16 +5,68 @@ module Day9
 
 import Lib
 
-type Day9Input = ()
+import Data.List
+import Data.List.Extra ((!?))
+import Data.Maybe
+import qualified Data.Set as S
 
-parseInput :: String -> Either () Day9Input
-parseInput = \x -> Right ()
+import Text.ParserCombinators.Parsec
+
+type Grid = [[Int]]
+
+type Day9Input = Grid
+
+parseLine :: Parser [Int]
+parseLine = do
+  xs <- many1 digit
+  optional newline
+  return $ map (read . (:[])) xs
+
+parseInput :: String -> Either ParseError Day9Input
+parseInput = parse (many parseLine) ""
 
 doDay9 :: IO ()
 doDay9 = doDay 9 parseInput part1 part2
 
+getInGrid :: Grid -> (Int,Int) -> Maybe Int
+getInGrid g (x,y) = do
+  r <- g !? x
+  r !? y
+
+getNeighbors :: Grid -> (Int,Int) -> [(Int,Int)]
+getNeighbors g (x,y) = filter f candidates
+  where
+    candidates = [(x+1,y), (x-1,y), (x,y+1),(x,y-1)]
+    w = length g
+    h = length $ head g
+    f (x,y) = x < w && x >= 0 && y < h && y >= 0
+
+getNeighborValues :: Grid -> (Int,Int) -> [Int]
+getNeighborValues g (x,y) = mapMaybe (getInGrid g) $ getNeighbors g (x,y)
+
+isLowPoint :: Grid -> (Int,Int) -> Bool
+isLowPoint g (x,y) = all (>v) $ getNeighborValues g (x,y)
+  where v = fromMaybe e $ getInGrid g (x,y)
+        e = error $ "Failed on " ++ (show (x,y))
+
+lowPointValues :: Grid -> [Int]
+lowPointValues g = mapMaybe (getInGrid g) lowpoints
+  where lowpoints = filter (isLowPoint g) $ allpoints g
+
+allpoints :: Grid -> [(Int,Int)]
+allpoints g = do
+  x <- init $ [0..(length g)]
+  y <- init $ [0..(length $ head g)]
+  return (x,y)
+
 part1 :: Day9Input -> Result Int
-part1 xs = NotImpl
+part1 g = Verified 570 res
+  where res = sum $ map (+1) $ lowPointValues g
+
+collectBasin :: Grid -> (Int,Int) -> [(Int,Int)]
+collectBasin g (x,y) = S.toList collected
+  where
+    collected = S.empty
 
 part2 :: Day9Input -> Result Int
 part2 xs = NotImpl
