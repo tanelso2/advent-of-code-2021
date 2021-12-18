b5f4f979cb1ad254349591a3c756be696d5b8cae
Thomas Nelson <speedster217@gmail.com>
2021-12-18 10:22:56 -0600

    Update Day16.hs

--- src/Day16.hs
+++ src/Day16.hs
@@ -5,10 +5,63 @@ module Day16
 
 import Lib
 
-type Day16Input = ()
+import Text.ParserCombinators.Parsec
+import ParseUtils
 
-parseInput :: String -> Either () Day16Input
-parseInput = \x -> Right ()
+type Day16Input = Packet
+
+data Packet = 
+   Literal PacketHeaders Int 
+   | Operator PacketHeaders [Packet]
+
+type PacketHeaders = (Int,Int)
+
+binNum :: Parser Char
+binNum = oneOf ['0','1']
+
+parseLiteralVal :: Parser Int
+parseLiteralVal = do
+   xs <- many middleGroup
+   x <- finalGroup
+   return readBinString $ xs++[x]
+
+middleGroup = do
+  char '0'
+  return $ ntimes 4 binNum
+
+finalGroup = do
+  char '1'
+  return $ ntimes 4 binNum
+
+headers :: Parser PacketHeaders
+headers = do
+  ver <- ntimes 3 binNum
+  type <- ntimes 3 binNum
+  return (readBinString ver, readBinString type)
+
+packet :: Parser Packet
+packet = do
+  (ver,type) <- headers
+  case type of
+    4 -> Literal (ver, type) $ parseLiteralVal
+    _ -> Operator (ver, type) $ operatorChildren
+
+operatorChildren :: Parser [Packet]
+operatorChildren = do
+  i <- binNum
+  if i == 0
+  then do
+    numBits <- liftM readBinString $ ntimes 15 binNum
+    thoseBits <- ntimes numBits binNum
+    case parse (many packet) "" thoseBits of
+      Left e -> unexpected "Inner parse failed..."
+      Right ps -> return ps
+  else do
+    numPackets <- liftM readBinString $ ntimes 11 binNum
+    ntimes numPackets packet
+
+parseInput :: String -> Either ParseError Day16Input
+parseInput s = parse packet "" $ hexToBin s
 
 doDay16 :: IO ()
 doDay16 = doDay 16 parseInput part1 part2
